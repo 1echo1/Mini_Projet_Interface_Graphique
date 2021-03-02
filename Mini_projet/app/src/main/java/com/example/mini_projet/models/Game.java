fa6f6e09e54e165bcf1d3ff4dafc31ca9a98d89d
1echo1 <vidalgemma01@gmail.com>
2021-03-02 19:18:51 +0100

    intent implicite -> google play

---
+++ Mini_projet/app/src/main/java/com/example/mini_projet/models/Game.java
@@ -0,0 +1,57 @@
+package com.example.mini_projet.models;
+
+import com.example.mini_projet.utils.Constants;
+
+import java.util.Random;
+
+public class Game {
+    public int[] array;
+    public int element_to_find;
+    public int total_choices;
+
+    public Game(int total_choices){
+        this.total_choices=total_choices;
+        this.array=new int[4];
+    }
+
+    public int get_cell(int indice){
+        return this.array[indice];
+    }
+
+    public void choose_elements_display(){
+        Boolean color_already_choose=false;
+        Random rand=new Random();
+        int rand_num;
+        int indice=0;
+
+        while(indice<4){
+            rand_num = rand.nextInt(this.total_choices);
+            for (int i = 0; i< this.array.length; i++){
+                if (rand_num== this.array[i]){
+                    color_already_choose=true;
+                }
+            }
+
+            if (!color_already_choose){
+                this.array[indice]=rand_num;
+                indice++;
+            }
+            color_already_choose=false;
+        }
+    }
+
+    public int get_element_to_find(){
+        Random rand=new Random();
+        int rand_num = rand.nextInt(Constants.NBR_CHOICES_IN_GAME-1);
+        element_to_find=this.array[rand_num];
+
+        return this.array[rand_num];
+    }
+
+    public Boolean verify_victory(int choosed){
+        if(choosed==this.element_to_find){
+            return true;
+        }
+        return false;
+    }
+}
