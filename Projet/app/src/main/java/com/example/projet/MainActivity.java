3ca1dd5a029cd47b82db6d7f77a3d97af372e12a
1echo1 <vidalgemma01@gmail.com>
2021-04-27 13:28:06 +0200

    Reprise projet de 0. Passage activités -> fragments

---
+++ Projet/app/src/main/java/com/example/projet/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    MenuFrag frag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
